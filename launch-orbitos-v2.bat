97ef053680f51e34d2ea1990bf5a0564faf0c4f2
Lars Eggimann <lars.eggimann@gmail.com>
2025-06-24 09:08:23 +0200

    use concurrently to launch stuff on windows

--- launch-orbitos-v2.bat
+++ launch-orbitos-v2.bat
@@ -1,22 +1,4 @@
 @echo off
-REM Script to launch backend (FastAPI) and frontend (npm) in production mode on Windows
-
-REM Start backend
-call .venv\Scripts\activate
-cd backend
-start "Backend" cmd /c "fastapi run src/main.py"
-cd ..
-
-REM Start frontend
+start firefox http://localhost:3000
 cd frontend
-start "Frontend" cmd /c "npm run start"
-cd ..
-
-REM Wait for user to press any key to stop processes
-echo.
-echo Press any key to stop backend and frontend...
-pause >nul
-
-REM Kill the backend and frontend windows
-taskkill /FI "WINDOWTITLE eq Backend" /T /F >nul 2>&1
-taskkill /FI "WINDOWTITLE eq Frontend" /T /F >nul 2>&1
+cmd /k "npm run win-run:all"
