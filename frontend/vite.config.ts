4321584a2fa702aa7df2bdf83e282670bdaff4f3
Lars Eggimann <lars.eggimann@gmail.com>
2025-07-04 14:25:57 +0200

    containerize frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import tsConfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   server: {
     port: 3000,
+    allowedHosts: ['irradiations-pc'],
   },
   plugins: [
     tsConfigPaths({
