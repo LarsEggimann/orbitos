1d39f7944a7853ea1b54a92e61dec61fc321d75d
LarsEggimann <lars.eggimann@gmail.com>
2025-07-16 01:28:45 +0200

    fix imports

--- frontend/src/routes/_pathlessLayout/chopperwheel.tsx
+++ frontend/src/routes/_pathlessLayout/chopperwheel.tsx
@@ -1,4 +1,4 @@
-import Chopperwheel from '@/components/chopperwheel/chopperwheel'
+import Chopperwheel from '~/components/chopperwheel/chopperwheel'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_pathlessLayout/chopperwheel')({
--- frontend/src/routes/_pathlessLayout/combo-data-view.tsx
+++ frontend/src/routes/_pathlessLayout/combo-data-view.tsx
@@ -1,4 +1,4 @@
-import ComboDataView from '@/components/combo-data-view/combo-data-view'
+import ComboDataView from '~/components/combo-data-view/combo-data-view'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_pathlessLayout/combo-data-view')({
