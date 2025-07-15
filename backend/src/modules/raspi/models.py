5863cd1c8d80d17394104806c892d1ce1380851d
LarsEggimann <lars.eggimann@gmail.com>
2025-07-15 17:54:47 +0200

    change path

--- backend/src/modules/raspi/models.py
+++ backend/src/modules/raspi/models.py
@@ -14,7 +14,7 @@ class RaspiSettings(BaseSetting, table=True):
 
     host: str = Field(default="raspi-server")
     port: int = Field(default=8000)
-    raspi_server_app_directory: str = Field(default="/home/lhep/orbitos/backend/src/modules/raspi/raspi_server")
+    raspi_server_app_directory: str = Field(default="~/orbitos/backend/src/modules/raspi/raspi_server")
 
 class RaspiSettingsSet(BaseModel):
     host: Optional[str] = None
